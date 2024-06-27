e0f44d261d1490a3b31ecce3d719350a51168e8e
Leanid Barok <tratos149@gmail.com>
2024-06-27 15:39:53 +0300

    clean up

--- src/main/java/com/github/semouse/kuy/six/equalsidesofarray/Kata.java
+++ src/main/java/com/github/semouse/kuy/six/equalsidesofarray/Kata.java
@@ -2,11 +2,20 @@ package com.github.semouse.kuy.six.equalsidesofarray;
 
 import java.util.Arrays;
 
-
+/**
+ * You are going to be given an array of integers. Your job is to take that array and find an index N where the sum of the integers to the left of N is equal to the sum of the integers to the right of N.
+ * <p>
+ * If there is no index that would make this happen, return -1.
+ */
 public class Kata {
 
+    private Kata() {
+        throw new IllegalStateException("Utility class");
+    }
+
     public static int findEvenIndex(int[] arr) {
-        int leftSum = 0, rightSum = Arrays.stream(arr).sum();
+        int leftSum = 0;
+        int rightSum = Arrays.stream(arr).sum();
 
         for (int i = 0; i < arr.length; i++) {
             rightSum -= arr[i];
