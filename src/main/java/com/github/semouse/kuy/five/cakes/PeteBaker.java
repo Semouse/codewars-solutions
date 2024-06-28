c95788852987e7928b70e7eff7f7c896f3bb3228
Leanid Barok <tratos149@gmail.com>
2024-06-28 17:05:59 +0300

    Add solution for Pete, the baker kata

--- src/main/java/com/github/semouse/kuy/five/cakes/PeteBaker.java
+++ src/main/java/com/github/semouse/kuy/five/cakes/PeteBaker.java
@@ -3,10 +3,26 @@ package com.github.semouse.kuy.five.cakes;
 
 import java.util.Map;
 
+/**
+ * Pete likes to bake some cakes. He has some recipes and ingredients.
+ * Unfortunately he is not good in maths.
+ * Can you help him find out, how many cakes he could bake considering his recipes?
+ * <p>
+ * Write a function cakes(), which takes the recipe (object)
+ * and the available ingredients (also an object)
+ * and returns the maximum number of cakes Pete can bake (integer).
+ * For simplicity there are no units for the amounts (e.g. 1 lb of flour or 200 g of sugar are simply 1 or 200).
+ * Ingredients that are not present in the objects, can be considered as 0.
+ */
 public class PeteBaker {
 
     public int cakes(Map<String, Integer> recipe, Map<String, Integer> available) {
-        // TODO: Insert Code Here
-        return -1;
+        int cakes = Integer.MAX_VALUE;
+        for (Map.Entry<String, Integer> ingredient : recipe.entrySet()) {
+            Integer amount = available.getOrDefault(ingredient.getKey(), 0);
+            int count = amount / ingredient.getValue();
+            cakes = Math.min(cakes, count);
+        }
+        return cakes;
     }
 }
